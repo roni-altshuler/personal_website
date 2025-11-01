6b1b8610582f25351c088f5941badfe7902e194e
Roni Altshuler <roni.altshuler@gmail.com>
2025-11-01 22:16:09 +0200

    Added the GitHub, email, and LinkedIn icons back to home landing page as the font awesome samplesheet was missing from previous upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        />
+      </head>
       <body suppressHydrationWarning={true}>
         <Navbar />
         {children}
