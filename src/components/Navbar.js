bf9a184c7a2e231ea373c02b2b7e12f6d61795c0
Roni Altshuler <roni.altshuler@gmail.com>
2025-11-01 22:46:01 +0200

    Updated website styling to be responsive to all devices, not just browsing on computer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
-
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
       <Link href="/" className={styles.logo}>Roni Altshuler</Link>
-      <div className={styles.navLinks}>
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
+      <div className={`${styles.navLinks} ${isOpen ? styles.navActive : ''}`}>
         <Link href="/">Home</Link>
         <Link href="/projects">Projects</Link>
         <Link href="/education">Education</Link>
